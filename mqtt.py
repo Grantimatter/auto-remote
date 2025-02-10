183cdf62cd826a11fda8e51e530f31e000a7939a
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 17:13:08 -0600

    typo fix

--- mqtt.py
+++ mqtt.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 controllers = []
 for remote in args.remotes:
     controller = CommandSet(Path(remote).stem, emitter_gpio=18, receiver_gpio=17, description=Path(remote).stem)
-    controller.load(args.remote)
+    controller.load(remote)
     controllers.append(controller)
 
 
