70df89daa5a1c6643bb6fd0d0c1a4656c2130ac5
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 13:40:05 -0600

    Added multiple powers for main.py

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ args = parser.parse_args()
 
 controller = CommandSet(args.name, emitter_gpio=18, receiver_gpio=17, description=args.name)
 
-# print('Press the power button on remote')
-# controller.add('power')
+print('Press the power button on remote')
+controller.add('power1')
+controller.add('power2')
+controller.add('power3')
+controller.add('power4')
+controller.add('power5')
+controller.add('power6')
+controller.add('power7')
+controller.add('power8')
+
 
 print('Press the Volume+ button on remote...')
 controller.add('volume_up')
