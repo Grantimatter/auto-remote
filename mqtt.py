3a9e33a5795664c15ced7def47d31d3f84721829
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 17:33:22 -0600

    logging

--- mqtt.py
+++ mqtt.py
@@ -30,18 +30,16 @@ def on_message(client, userdata, msg):
             if payload == 'power_on':        
                 controller.emit('power_on1')
                 controller.emit('power_on2')
-                controller.emit('power_on3')
                 print('Emitting Power ON')
             elif payload == 'power_off':
                 controller.emit('power_off1')
                 controller.emit('power_off2')
-                controller.emit('power_off3')
                 print('Emitting Power OFF')
             else:
                 controller.emit(payload)
                 print(f'Emitting {payload.upper()}')
-        except:
-            print(f'Error emitting {payload}')
+        except Exception as e:
+            print(f'Error emitting {payload}: {e}')
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
