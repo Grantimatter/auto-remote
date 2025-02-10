9062a19bd648e8fc5d9d91b6b60e06d2f5961ec0
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 13:37:27 -0600

    Removed power from main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ args = parser.parse_args()
 
 controller = CommandSet(args.name, emitter_gpio=18, receiver_gpio=17, description=args.name)
 
-print('Press the power button on remote')
-controller.add('power')
+# print('Press the power button on remote')
+# controller.add('power')
 
 print('Press the Volume+ button on remote...')
 controller.add('volume_up')
