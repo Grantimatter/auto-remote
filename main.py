c3c8919b0083244dfa781bb263ea50e60de9169d
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 10:21:32 -0600

    Added button prompts

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from ircodec.command import CommandSet
 
 controller = CommandSet('hisense-remote', emitter_gpio=18, receiver_gpio=17, description='Hisense TV Remote')
 
+print('Press the power button on remote')
+controller.add('power')
 
+print('Press the Volume+ button on remote...')
+controller.add('volume_up')
+
+print('Press the Volume- button on remote')
+controller.add('volume_down')
 
 
 controller.save_as('hisense-remote.json')
