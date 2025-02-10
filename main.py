8dabe9616cae98d4b79333aed55db57d58d73ae0
Grant Wiswell <wiswellgrant@gmail.com>
2025-02-10 13:41:25 -0600

    Updated main.py to receive 3 power codes

--- main.py
+++ main.py
@@ -11,12 +11,6 @@ print('Press the power button on remote')
 controller.add('power1')
 controller.add('power2')
 controller.add('power3')
-controller.add('power4')
-controller.add('power5')
-controller.add('power6')
-controller.add('power7')
-controller.add('power8')
-
 
 print('Press the Volume+ button on remote...')
 controller.add('volume_up')
